f1e6e8b77ca669b8028ba9ae5b9265b287dd3c28
user2802 <100025896+user2802@users.noreply.github.com>
2023-10-17 18:44:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,26 +26,69 @@ recognizer = sr.Recognizer()
 # Path for google text to speach api key
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
 
+# Setting up OpenAI API key
+with open('openai_api.json', 'r') as file:
+    config = json.load(file)
+    openai.api_key = config['OPENAI_API_KEY']
 
 def recognize_speech_from_mic():
+    print("Hello! You can start speaking now. Say 'hello hi' to start the conversation.")
     with sr.Microphone() as source:
         while True:
             print("Listening for 'hello hi' trigger...")
-            audio = recognizer.listen(source, timeout=10, phrase_time_limit=5)  # Listen for short bursts
+            audio = recognizer.listen(source, timeout=10, phrase_time_limit=5)
             try:
-                # Attempt to recognize the audio
                 speech_text = recognizer.recognize_google(audio)
                 print(f"Heard: {speech_text}")
                 if speech_text.lower().startswith("hello hi"):
-                    print("Trigger detected! Please continue with your question.")
-                    # Now we listen for the actual question
-                    audio_question = recognizer.listen(source)
-                    question_text = recognizer.recognize_google(audio_question)
-                    return question_text[len("hello hi "):].strip()
+                    print("Trigger detected! Entering conversation mode.")
+                    conversation_mode(source)
             except sr.UnknownValueError:
-                # If Google Speech Recognition couldn't understand the audio
+                # Google Speech Recognition couldn't understand the audio
                 pass
 
+def conversation_mode(source):
+    conversation_history = ""  # Initialize an empty conversation history for this session
+    while True:
+        print("Listening for your question...")
+        audio = recognizer.listen(source)
+        try:
+            user_input = recognizer.recognize_google(audio)
+            print(f"You said: {user_input}")
+
+            if user_input.lower() in ["goodbye", "stop"]:
+                speak_text_google("Goodbye!")
+                print("Goodbye!")
+                return  # Exiting the conversation mode
+
+            # Add user's input to the conversation history
+            conversation_history += f"User: {user_input}\n"
+
+            # Interact with OpenAI API
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=conversation_history + "AI:",
+                max_tokens=150,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+
+            gpt_response = response.choices[0].text.strip()
+            print("Response:", gpt_response)
+
+            # Add AI's response to the conversation history
+            conversation_history += f"AI: {gpt_response}\n"
+
+            # Log the conversation to a file
+            log_filename = "conversation_log.txt"  # This can be moved outside of the function if you don't want a new file for each session
+            log_conversation_to_file(log_filename, user_input, gpt_response)
+
+            # Speak the response using Google Cloud's Text-to-Speech
+            speak_text_google(gpt_response)
+
+        except sr.UnknownValueError:
+            print("Sorry, I didn't catch that. Please try again or say 'goodbye' to exit.")
 
 
 def speak_text_google(text):
@@ -82,53 +125,7 @@ def log_conversation_to_file(log_filename, user_input, gpt_response):
         f.write(f"AI: {gpt_response}\n\n")
 
 
-def main():
-    with open('openai_api.json', 'r') as file:
-        config = json.load(file)
-        openai.api_key = config['OPENAI_API_KEY']
-
-    print("Hello! You can start speaking now. Say 'goodbye' to end the conversation.")
-
-    conversation_history = ""
-    log_filename = "conversation_log.txt"
-
-    while True:
-        user_input = recognize_speech_from_mic()
-
-        if user_input:
-            print(f"You said: {user_input}")
-
-            if user_input.lower() in ["goodbye", "stop"]:
-                speak_text_google("Goodbye!")
-                print("Goodbye!")
-                break
-
-            # Add user's input to the conversation history
-            conversation_history += f"User: {user_input}\n"
-
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=conversation_history + "AI:",
-                max_tokens=150,
-                n=1,
-                stop=None,
-                temperature=0.5,
-            )
-
-            gpt_response = response.choices[0].text.strip()
-            print("Response:", gpt_response)
-
-            # Add AI's response to the conversation history
-            conversation_history += f"AI: {gpt_response}\n"
-
-            # Log the conversation to a file
-            log_conversation_to_file(log_filename, user_input, gpt_response)
-
-            # Speak the response using Google Cloud's Text-to-Speech
-            speak_text_google(gpt_response)
-        else:
-            print("Sorry, I didn't catch that. Please try again or say 'goodbye' to end the conversation.")
-
 
 if __name__ == '__main__':
-    main()
+    recognize_speech_from_mic()
+
